2b33c40a738f6b2fc51a07d75285286f56f836a1
MinMiguelM <cmiguelmg@gmail.com>
2018-04-04 19:07:38 -0500

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import NotFound from "./components/NotFound";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route component={NotFound} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
